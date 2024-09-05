635220913cffb9f091b0f5d8cc27d27443bd1b81
illudd <chominjung821@naver.com>
2024-09-05 21:55:04 +0900

    feat: bingo main

--- bff/src/Home/BingoMain.tsx
+++ bff/src/Home/BingoMain.tsx
@@ -1,13 +1,26 @@
+import React from 'react';
 import '../style/Home/bingomain.scss';
 
-function BingoMain() {
-
+const BingoMain: React.FC = () => {
   return (
-    <>
-
-    </>
+    <div className="bingo-container">
+      <div className="bingo-header">
+        <h2>제목없음</h2>
+        <p>(팀명)의 빙고</p>
+      </div>
+      <div className="bingo-background">
+      <div className="bingo-grid">
+        {Array(9).fill(null).map((_, index) => (
+          <div key={index} className="bingo-cell"></div>
+        ))}
+        </div>
+      </div>
+      <div className="bingo-footer">
+        <p>빙고칸을 눌러 빙고판을 채워보세요</p>
+        <button className="download-button">⬇️</button>
+      </div>
+    </div>
   );
-}
+};
 
 export default BingoMain;
-
--- bff/src/Home/Home.tsx
+++ bff/src/Home/Home.tsx
@@ -1,16 +1,34 @@
+// import '../style/Home/home.scss';
+// import LeftMenu from './LeftMenu';
+// import RightMenu from './RightMenu';
+// import BingoMain from './BingoMain';
+
+// function Home() {
+//   const containerStyle = {
+//     height: '100%',
+//     width: '100%',
+//   }
+
+//   return (
+//     <div style={containerStyle}>
+//       <LeftMenu />
+//       <BingoMain />
+//       <RightMenu />
+//     </div>
+//   );
+// }
+
+// export default Home;
+
+import React from 'react';
 import '../style/Home/home.scss';
 import LeftMenu from './LeftMenu';
 import RightMenu from './RightMenu';
 import BingoMain from './BingoMain';
 
 function Home() {
-  const containerStyle = {
-    height: '100%',
-    width: '100%',
-  }
-
   return (
-    <div style={containerStyle}>
+    <div className="home-container">
       <LeftMenu />
       <BingoMain />
       <RightMenu />
@@ -19,4 +37,3 @@ function Home() {
 }
 
 export default Home;
-
